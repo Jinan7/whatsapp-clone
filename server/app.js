faba0a33527dc2fa8df32efe008e734ab3028f01
Jinan Lakan <lakanj25@gmail.com>
2023-08-30 04:18:24 +0200

    Add router

--- server/app.js
+++ server/app.js
@@ -1,14 +1,22 @@
+require('express-async-errors')
 const express = require('express')
 const dotenv = require('dotenv')
 const connect = require('./db/connect')
-
+const { msgrouter } = require('./routers/messageRouter')
+const Cors = require('cors')
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 5000
 const url = process.env.MONGO_URI
 
+app.use(express.json())
+app.use(Cors())
+app.use('/api/v1/messages', msgrouter)
 
+app.use(async (err, req, res, next)=>{
+    res.send(err.message)
+})
 
 
 
